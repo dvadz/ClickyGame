69c1e2197b1a693f020154053d2ddee174ad0ffc
Daniel Vadil <dvadz@yahoo.com>
2019-06-20 23:22:35 -0400

    - removed unneeded files - added a folder of images - added test base component that simply says 'Hello World'

---
+++ src/components/ClickyGame.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ClickyGame extends Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default ClickyGame;
