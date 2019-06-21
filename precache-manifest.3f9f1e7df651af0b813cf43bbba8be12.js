0403c12985e2d8c81c341869df5f07c0fb711426
Daniel Vadil <dvadz@yahoo.com>
2019-06-21 15:26:33 -0400

    Updates

---
+++ precache-manifest.3f9f1e7df651af0b813cf43bbba8be12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85bdadf1bfc13f168c5baea93f47ae24",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "0302fa2debda1ce3f228",
+    "url": "/ClickyGame/static/css/main.bb61a31d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ClickyGame/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "0302fa2debda1ce3f228",
+    "url": "/ClickyGame/static/js/main.6769d91b.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
