c1ed722ef7bdf0b78555326b600c0ee516081c7b
Daniel Vadil <dvadz@yahoo.com>
2019-06-24 19:53:00 -0400

    - assigned the click event from the card component to the img - played with the card and image css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import "./Card.css";
 
 export default function Card(props) {
         return (
-            <div className="card col-3" onClick={props.onCardClick}>
-                <img src={props.src} alt="fish"/>
+            <div className="card col-3">
+                <img src={props.src} alt="fish" onClick={props.onCardClick} />
             </div>
         )
 }
